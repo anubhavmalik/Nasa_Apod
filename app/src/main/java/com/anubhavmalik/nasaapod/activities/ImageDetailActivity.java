94c7b1f64ee8ce428f2f193c60de55225ae2c799
anubhavmalik <anubhavmalikdeveloper@gmail.com>
2020-01-28 03:38:33 +0530

    image detail layout and fragment.

---
+++ app/src/main/java/com/anubhavmalik/nasaapod/activities/ImageDetailActivity.java
@@ -0,0 +1,16 @@
+package com.anubhavmalik.nasaapod.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anubhavmalik.nasaapod.R;
+
+public class ImageDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detail);
+    }
+}
